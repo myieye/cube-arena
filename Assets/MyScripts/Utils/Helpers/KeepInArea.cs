35ed32570c830b549b15f35012f6052bfb7cea04
myieye <myieye@gmail.com>
2018-05-03 12:11:11 +0200

    Initial KeepInArea code.

---
+++ Assets/MyScripts/Utils/Helpers/KeepInArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CubeArena.Assets.MyScripts.Utils.Helpers {
+	public class KeepInArea : MonoBehaviour {
+		
+		[SerializeField]
+		private Collider area;
+		private float radius;
+
+		void Start () {
+			radius = area.bounds.center.x - area.bounds.min.x;
+		}
+
+		void Update () {
+			var posRelativeToOrigin = Vector3.ClampMagnitude(transform.position - area.bounds.center, radius);
+			transform.position = posRelativeToOrigin + area.bounds.center;
+		}
+	}
+}
