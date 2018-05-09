3b0a1fb60ca782bf3f60f79df1a40415823e078a
myieye <myieye@gmail.com>
2018-05-09 15:46:09 +0200

    Make KeepInArea safer with RequireComponent.

--- Assets/MyScripts/Utils/Helpers/KeepInArea.cs
+++ Assets/MyScripts/Utils/Helpers/KeepInArea.cs
@@ -4,10 +4,11 @@ using CubeArena.Assets.MyScripts.Utils.TransformUtils;
 using UnityEngine;
 
 namespace CubeArena.Assets.MyScripts.Utils.Helpers {
+	[RequireComponent (typeof (BoxCollider))]
 	public class KeepInArea : MonoBehaviour {
 
 		void Update () {
-			TransformUtil.ClampInArea(transform, GetComponent<BoxCollider>().size.x / 2);
+			TransformUtil.ClampInArea (transform, GetComponent<BoxCollider> ().size.x / 2);
 		}
 	}
 }
