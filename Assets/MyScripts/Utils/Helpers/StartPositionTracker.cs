78e39e75a5cef60f0c65bf3b7cda12eaed380e4d
myieye <myieye@gmail.com>
2018-06-07 17:36:52 +0200

    Fix tracking wrong player start position.

--- Assets/MyScripts/Utils/Helpers/StartPositionTracker.cs
+++ Assets/MyScripts/Utils/Helpers/StartPositionTracker.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using CubeArena.Assets.MyScripts.Logging;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace CubeArena.Assets.MyScripts.Utils.Helpers {
-	public class StartPositionTracker : MonoBehaviour {
+	public class StartPositionTracker : NetworkBehaviour {
 
 		public Vector3 StartPosition { get; private set; }
 
 		void Start () {
 			StartPosition = transform.position;
+		}
+
+		public override void OnStartAuthority () {
+			base.OnStartAuthority ();
 			Measure.LocalInstance.StartPosition = StartPosition;
 		}
 	}
